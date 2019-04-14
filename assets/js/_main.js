4fd0ede46e5f6d08e12199557c39784d1acfbdee
Kulbhushan-Chand <kulbhushan.chand@gmail.com>
2019-04-14 18:55:41 +0530

    Theme update

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -76,22 +76,24 @@ $(document).ready(function() {
   });
   
   // Gumshoe scroll spy init
-  var spy = new Gumshoe("nav.toc a", {
-    // Active classes
-    navClass: "active", // applied to the nav list item
-    contentClass: "active", // applied to the content
+  if($("nav.toc").length > 0) {
+    var spy = new Gumshoe("nav.toc a", {
+      // Active classes
+      navClass: "active", // applied to the nav list item
+      contentClass: "active", // applied to the content
 
-    // Nested navigation
-    nested: false, // if true, add classes to parents of active link
-    nestedClass: "active", // applied to the parent items
+      // Nested navigation
+      nested: false, // if true, add classes to parents of active link
+      nestedClass: "active", // applied to the parent items
 
-    // Offset & reflow
-    offset: 20, // how far from the top of the page to activate a content area
-    reflow: true, // if true, listen for reflows
+      // Offset & reflow
+      offset: 20, // how far from the top of the page to activate a content area
+      reflow: true, // if true, listen for reflows
 
-    // Event support
-    events: true // if true, emit custom events
-  });
+      // Event support
+      events: true // if true, emit custom events
+    });
+  }
   
 
  // add lightbox class to all image links
